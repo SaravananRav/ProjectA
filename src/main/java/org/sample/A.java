6fd1c8a4ae476416322654913b31730d456325a1
SaravananRav <smartsaran006@gmail.com>
2019-11-07 21:39:46 +0530

    I have done my login functionality

---
+++ src/main/java/org/sample/A.java
@@ -0,0 +1,17 @@
+package org.sample;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import com.google.common.graph.ElementOrder.Type;
+
+public class A extends BaseClass {
+	public static void main(String[] args) {
+		getDriver();
+       loadurl("https://www.facebook.com/");
+       WebElement txt = driver.findElement(By.id("email"));
+      
+       type(txt,"smartsaran");
+	}
+
+}
---
+++ src/main/java/org/sample/BaseClass.java
@@ -0,0 +1,22 @@
+package org.sample;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseClass {
+	public static WebDriver driver;
+	public static WebDriver getDriver(){
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\smart saran\\Downloads\\eclipse-committers-oxygen-3a-win32\\eclipse\\saro\\New\\driverr\\chromedriver.exe");
+		 return driver=new ChromeDriver();
+		
+	}
+	public static void loadurl(String url) {
+    driver.get(url);
+	}
+	public static void type(WebElement e,String value) {
+		e.sendKeys(value);
+
+	}
+
+}
